907f110b841b51d4d211b23d75b7650e64c4b9b4
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-15 14:47:05 +0300

    adding commenting route

--- app/main/views.py
+++ app/main/views.py
@@ -47,4 +47,31 @@ def write_blog():
 
 
     return render_template('blogs.html',form = form)
+
+@main.route('/user/<uname>/update/pic',methods= ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+        return redirect(url_for('auth.profile',uname=uname)) 
+
+@main.route('/comment/<comment_id>/add/comment', methods = ['GET','POST'])
+@login_required
+def newComment(comment_id):
+    print(comment_id)
+    form = CommentForm() 
+    if form.validate_on_submit():
+        about = form.about.data 
+        new_comment = Comment(about=about,writer=current_user.id,post=comment_id)
+        db.session.add(new_comment)
+        db.session.commit()
+       
+        return redirect(url_for('main.index'))
   
+    return render_template('auth/comments.html',comment_form=form)
+
+ 
