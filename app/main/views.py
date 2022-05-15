ae856ddee9452deebb14cfeadfeffc84fd259107
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-15 14:55:30 +0300

    adding configurations for development

--- app/main/views.py
+++ app/main/views.py
@@ -21,12 +21,9 @@ def update_profile(uname):
     user = User.query.filter_by(username = uname).first()
     if user is None:
         abort(404)
-
     form = UpdateProfile()
-
     if form.validate_on_submit():
         user.bio = form.bio.data
-
         db.session.add(user)
         db.session.commit()
 
--- config.py
+++ config.py
@@ -1,16 +1,25 @@
 import os
 import re
 class Config:
-   
+   SQLCHEMY_TRACK_MODIFICATIONS = True 
    QUOTE_BASE_URL="http://quotes.stormconsultancy.co.uk/random.json"
    SECRET_KEY = os.environ.get("SECRET_KEY") 
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:cheeks@localhost/blogs'
+   UPLOADED_PHOTOS_DEST = 'app/static/photos'
+   MAIL_SERVER = 'smtp.googlemail.com'
+   MAIL_PORT = 587
+   MAIL_USE_TLS = True
+   MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+   MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
 
 
 class ProdConfig(Config):
-    pass 
+      SQLALCHEMY_DATABASE_URI= os.environ.get("DATABASE_URL")
+      if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
+        SQLALCHEMY_DATABASE_URI=SQLALCHEMY_DATABASE_URI.replace('postgres://', 'postgresql://')
 
 class DevConfig(Config):
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:cheeks@localhost/blogs' 
     DEBUG = True
 
 
