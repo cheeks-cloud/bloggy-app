12dc8a28d644740b682bb1c46f30bcb63fe7ed0b
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-15 14:13:10 +0300

    creating a comment model

--- app/models.py
+++ app/models.py
@@ -38,22 +38,37 @@ class User(UserMixin,db.Model):
   def load_user(user_id):
     return User.query.get(int(user_id)) 
 
-# class Blog(db.Model):
-#     __tablename__ = 'blogs'
-#     id = db.Column(db.Integer, primary_key=True)
-#     blog = db.Column(db.Text,nullable=False)
-#     author = db.Column(db.Integer,db.ForeignKey('users.id'))
-#     comment = db.relationship('Comment', backref='posts',lazy='dynamic')
+class Blog(db.Model):
+    __tablename__ = 'blogs'
+    id = db.Column(db.Integer, primary_key=True)
+    blog = db.Column(db.Text,nullable=False)
+    author = db.Column(db.Integer,db.ForeignKey('users.id'))
+    comment = db.relationship('Comment', backref='posts',lazy='dynamic')
 
-#     def get_blogs():
-#         blogs = Blog.query.all()
-#         return blogs
+    def get_blogs():
+        blogs = Blog.query.all()
+        return blogs
 
-#     def get_author(self,id):
-#         author = User.query.filter_by(id=id).first()
-#         return author.username
+    def get_author(self,id):
+        author = User.query.filter_by(id=id).first()
+        return author.username
 
-#     def get_comments(self,blog):
-#         all_comments = Comment.query.filter_by(blog=blog).all()
-#         return all_comments
+    def get_comments(self,blog):
+        all_comments = Comment.query.filter_by(blog=blog).all()
+        return all_comments
 
+class Comment(db.Model):
+    __tablename__ = 'comment'
+    id = db.Column(db.Integer,primary_key=True)
+    about=db.Column(db.Text,nullable=False)
+    post=db.Column(db.Integer,db.ForeignKey('posts.id'))
+    writer = db.Column(db.Integer,db.ForeignKey('users.id'))
+   
+   
+    def get_writer(self,id):
+        writer = User.query.filter_by(id=id).first()
+        return writer.username
+
+    def get_post(self):
+        post = Post.query.filter_by(id=self.id).first()
+        return post
