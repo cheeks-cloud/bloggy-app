8ebda035843657ed4e6e485c612661024cf9707f
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-15 09:25:05 +0300

    creating a quote class

--- app/models.py
+++ app/models.py
@@ -5,4 +5,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 # from . import login_manager
 
 class Quote:
-  def __init__(self,id,quote,author)
+  def __init__(self,id,quote,author):
+    self.id = id
+    self.quote = quote
+    self.author = author
--- app/request.py
+++ app/request.py
@@ -1,5 +1,5 @@
 import urllib.request,json
-# from .models import 
+from .models import Quote
 api_key = None
 base_url = None
 
@@ -7,3 +7,31 @@ def configure_request(app):
   global api_key,base_url
   api_key = app.config['']
   base_url = app.config['']
+
+# def get_quotes():
+#   get_quotes_url = base_url .format(x,api_key)
+
+#   with urllib.request.urlopen(get_quotes_url)as url:
+#     get_quotes_data = url.read()
+#     get_quotes_reponse = json.loads(get_quotes_data) 
+#     quotes_results = None
+
+#     if get_quotes_reponse['results']:
+#       quotes_results_list = get_quotes_reponse['results']
+#       quotes_results  = process_results(quotes_results_list)
+
+#   return quotes_results
+
+
+# def process_results(quote_list):
+#   quotes_results = []
+#   for quote_item in quote_list:
+#     id =  quote_item.get('id')
+#     quote = quote_item.get('quote')
+#     author = quote_item.get('author')
+   
+#     if id:
+#       quote_object = Quote(id,quote,author)
+#       quotes_results.append(quote_object)
+
+#   return quotes_results 
