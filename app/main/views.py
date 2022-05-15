018ce1c4496f19205513cd53639ad80489000ba4
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-15 09:33:17 +0300

    creating base.html and navbar

--- app/main/views.py
+++ app/main/views.py
@@ -1,11 +1,13 @@
 from flask import render_template,request,redirect,url_for
 from . import main
-# from ..requests import get_movies,get_movie,search_movie
+from  app.request import get_quotes
 # from .forms import ReviewForm
 # from ..models import Review
 
 @main.route('/')
 def index():
+    title = "Welcome to Blogging Application"
 
+    quotes_results = get_quotes()
   
-  return render_template('index.html')
+    return render_template('index.html',title=title,quotes = quotes_results)
