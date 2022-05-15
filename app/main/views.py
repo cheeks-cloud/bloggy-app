4075dd482f2b849e70c6cfc92f292673a0548e94
@cheeks cloud <agneskaswii700@gmail.com>
2022-05-15 14:41:21 +0300

    adding profile update route

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from  app.request import get_quotes
-# from .forms import ReviewForm
-# from ..models import Review
+from ..models import Comment,User,Blog
+from .forms import CommentForm,UpdateProfile,BlogForm
+from flask_login import login_required,current_user
+from .. import db,photos
 
 @main.route('/')
 def index():
@@ -11,3 +13,24 @@ def index():
     quoteFound = get_quotes()
   
     return render_template('index.html',title=title,quotes = quoteFound)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('main.update_profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
+  
